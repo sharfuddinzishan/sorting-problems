18aace3d580d253643754946bc8050c5e9be42d9
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-02-27 04:29:29 +0600

    Heap Completed

--- heapify.c
+++ heapify.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-int data[] = {};
+int data[50] = {};
 int size;
 void getInputs()
 {
-    int n, i = 0;
+    int n, i = 1;
     printf("How Many Numbers You Want?");
     scanf("%d", &n);
     size = n;
     printf("\nEnter Numbers By Spacing or Pressing ENTER");
-    while (n)
+    while (n > 0)
     {
         printf("\nEnter Numbers[%d]: ", i);
         scanf("%d", &data[i]);
@@ -29,19 +29,48 @@ void show(char action[])
     {
         printf("\nAfter Sorting: ");
     }
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i <= size; i++)
     {
         printf("%d ", data[i]);
     }
 }
-void heapify(int arr[], int end, int i)
+void swap(int *a, int *b)
 {
-    int max = i;
-    int left = i * 2;
-    int right = i * 2 + 1;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapify(int arr[], int end, int index)
+{
+    int max = index;
+    int left = index * 2;
+    int right = (index * 2) + 1;
+    while (left <= end & arr[max] < arr[left])
+        max = left;
+    while (right <= end & arr[max] < arr[right])
+        max = right;
+    if (max != index)
+    {
+        swap(&arr[index], &arr[max]);
+        heapify(arr, end, max);
+    }
+}
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2; i >= 1; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for (int i = n; i >= 1; i--)
+    {
+        swap(&arr[1], &arr[i]);
+        heapify(arr, i - 1, 1);
+    }
 }
 int main()
 {
     getInputs();
     show("Prev");
+    heapSort(data, size);
+    show("Next");
 }
